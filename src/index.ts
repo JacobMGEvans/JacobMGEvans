c4b1b72fdcf84980aff95f23b5fe140bdad08697
JacobMGEvans <jacobmgevans@gmail.com>
2023-03-27 00:10:34 -0500

    Started the setting up for the tailwind stuff

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,37 @@
-export default {
-  async fetch(request: Request): Promise<Response> {
+const handler = {
+  async fetch(): Promise<Response> {
     const headers = new Headers();
     headers.set("Content-Type", "text/html;charset=UTF-8");
 
-    const readmeContent = await (
-      await fetch(
-        "https://raw.githubusercontent.com/JacobMGEvans/JacobMGEvans/main/README.md"
-      )
-    ).text();
+    const readmeContent = await fetch(
+      "https://raw.githubusercontent.com/JacobMGEvans/JacobMGEvans/main/readme.html"
+    );
+    /**
+     * TODO: Create a slicable/gettable Head from README and drop it into template <head> tag here
+     * TODO: TailwindCSS needs to be accessible on the website as an asset AND locally for the README.md file
+     * TODO: Use HTMLRewriter to add the TailwindCSS stylesheet to the <head> tag and replace this template literal.
+     */
+    // const transformedHtml = new HTMLRewriter({ html: true })
+    //   .on("head", {
+    //     element(element) {
+    //       element.append(
+    //         `<meta property="og:url" content="https://dev.to/jacobmgevans" />`,
+    //         `<meta name="description" content="Jacob MG Evans GitHub profile. Describing personal passions and accomplishments."/>`,
+    //         `<meta name="viewport" content="width=device-width, initial-scale=1" />`,
+    //         `<meta name="theme-color" content="#000000" />`,
+    //         `<title> JacobMGEvans profile </title>`
+    //       );
+    //     },
+    //   })
+    //   .on("body", {
+    //     element(element) {
+    //       element.setAttribute("align", "center");
+    //     },
+    //   });
 
-    //TODO Create a slicable/gettable Head from README and drop it into template <head> tag here
-    // TODO: TailwindCSS needs to be accessible on the website as an asset AND locally for the README.md file
-    const html = `
-    <!DOCTYPE html>
-    <head>
-      <meta property="og:url" content="https://dev.to/jacobmgevans" />
-      <meta name="description" content="Jacob MG Evans GitHub profile. Describing personal passions and accomplishments."/>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#000000" />
-      <title> JacobMGEvans profile </title> 
-    </head>
-    <html lang="en">
-        <body align="center">
-            ${readmeContent}
-        </body>
-    </html>
-    `;
-
-    console.log(JSON.stringify(readmeContent));
-    return new Response(html, { headers });
+    // return transformedHtml.transform(readmeContent);
+    return new Response(await readmeContent.text(), { headers });
   },
 };
+
+export default handler;
