bb6898d8c5811f2881d598e1972f9d6d7473c1fc
Jacob MG Evans <jacobmgevans@gmail.com>
2025-04-17 20:28:17 -0700

    Fixed the routing somewhat

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,20 @@
 import { flex, gradientHeader, link, socialIcon } from '../css-utilities';
 
 export function HeaderComponent() {
+  const pathname = window.location.pathname;
+  const isOnBlogPage = pathname === '/blog';
+
+  const getNavLink = (section: string) => {
+    if (section === 'blog') {
+      return isOnBlogPage ? '/' : '/blog';
+    }
+
+    if (section.startsWith('#')) {
+      return isOnBlogPage ? `/${section}` : section;
+    }
+
+    return section;
+  };
   return (
     <header
       class={`sticky top-0 z-50 w-full p-4 ${gradientHeader()} backdrop-blur-md border-b border-gray-800 text-gray-100 flex flex-col md:flex-row justify-between items-center`}
@@ -41,22 +55,22 @@ export function HeaderComponent() {
       <nav class="mt-4 md:mt-0">
         <ul class={flex('row', 'space-x-6')}>
           <li>
-            <a href="/#about" class={link('nav')}>
+            <a href={getNavLink('#about')} class={link('nav')}>
               About
             </a>
           </li>
           <li>
-            <a href="/#oss" class={link('nav')}>
+            <a href={getNavLink('#oss')} class={link('nav')}>
               OSS & Community
             </a>
           </li>
           <li>
-            <a href="/blog" class={link('nav')}>
+            <a href={getNavLink('/blog')} class={link('nav')}>
               Blog
             </a>
           </li>
           <li>
-            <a href="/#outdoor" class={link('nav')}>
+            <a href={getNavLink('#outdoor')} class={link('nav')}>
               Outdoor Life
             </a>
           </li>
