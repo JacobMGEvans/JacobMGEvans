4502143214522383153d8c664ff8d4b759640c32
Jacob MG Evans <jacobmgevans@gmail.com>
2025-04-29 16:33:16 -0700

    Add Vite with Cloudflare & Tailwind plugin

---
+++ tailwind.config.ts
@@ -0,0 +1,46 @@
+import type { Config } from 'tailwindcss';
+import animate from 'tailwindcss-animate';
+const config: Config = {
+  darkMode: ['class', '[data-theme="dark"]'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    '*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        wolf: {
+          gray: '#4B5563',
+          dark: '#1E1E2F',
+          light: '#9CA3AF',
+        },
+        forest: {
+          green: '#182428',
+          light: '#293D36',
+          dark: '#0C1A15',
+        },
+        mountain: {
+          blue: '#00F0FF',
+          purple: '#DF00FE',
+        },
+        cyber: {
+          pink: '#FC28FB',
+          blue: '#03EDF9',
+          yellow: '#FEF851',
+          green: '#39FF14',
+          red: '#FF003C',
+        },
+      },
+      fontFamily: {
+        sans: ['Rajdhani', 'sans-serif'],
+        heading: ['Orbitron', 'sans-serif'],
+        body: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [animate],
+};
+
+export default config;
---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import { cloudflare } from '@cloudflare/vite-plugin';
+
+export default defineConfig({
+  plugins: [tailwindcss(), cloudflare()],
+});
