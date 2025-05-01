ddbc1c419ac60319fc31be2b9ecbf59186003faa
Jacob MG Evans <jacobmgevans@gmail.com>
2025-04-30 19:22:03 -0700

    Added Hono React plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import { cloudflare } from '@cloudflare/vite-plugin';
+import reactStack from 'hono-vite-react-stack';
 
 export default defineConfig({
-  plugins: [tailwindcss(), cloudflare()],
+  plugins: [tailwindcss(), cloudflare(), reactStack()],
 });
