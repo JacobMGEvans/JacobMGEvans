0b43cc2266a6172a66cfe4807c0b5fd997e95cae
Jacob MG Evans <jacobmgevans@gmail.com>
2025-07-05 13:49:06 -0700

    feat: update presence API

--- src/server/api/presence.ts
+++ src/server/api/presence.ts
@@ -6,12 +6,12 @@ interface Env {
 }
 
 // Proxy /api/presence requests (HTTP & WebSocket) to the Durable Object
-// export async function handlePresence(
-//   c: Context<{ Bindings: Env }>
-// ): Promise<Response> {
-//   const id = c.env.PRESENCE.idFromName('global');
-//   const stub = c.env.PRESENCE.get(id);
-//
-//   // @ts-expect-error: allow raw CF Request
-//   return stub.fetch(c.req.raw as any);
-// }
+export async function handlePresence(
+  c: Context<{ Bindings: Env }>
+): Promise<Response> {
+  const id = c.env.PRESENCE.idFromName('global');
+  const stub = c.env.PRESENCE.get(id);
+
+  // @ts-expect-error: allow raw CF Request
+  return stub.fetch(c.req.raw as any);
+}
